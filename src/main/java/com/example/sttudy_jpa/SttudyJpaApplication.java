e81c9fb6153134379e5d005a6755dedf62278f21
novb1492 <novb1492@naver.com>
2022-04-03 20:38:24 +0900

    first

---
+++ src/main/java/com/example/sttudy_jpa/SttudyJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sttudy_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttudyJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttudyJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sttudy_jpa/SttudyJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sttudy_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttudyJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
