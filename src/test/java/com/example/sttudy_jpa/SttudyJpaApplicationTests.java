d3f156bea881f282228414a59859b1bb9a0223b8
novb1492 <novb1492@naver.com>
2022-04-04 20:07:17 +0900

    ++

--- src/test/java/com/example/sttudy_jpa/SttudyJpaApplicationTests.java
+++ src/test/java/com/example/sttudy_jpa/SttudyJpaApplicationTests.java
@@ -24,24 +24,6 @@ class SttudyJpaApplicationTests {
 
 
 	@Test
-	void test(){
-		QaTable qaTable=QaTable.aTable;
-		QbTable qbTable=QbTable.bTable;
-		//select a.*,b.* from test.atable a inner join  test.btable b on b.a_table_a_id=a.a_id where a.a_id=1
-		List<aTable>aTables=jpaQueryFactory.selectFrom(qaTable).leftJoin(qbTable).fetchJoin().on(qbTable.aTable.aId.eq(qaTable.aId)).where(qaTable.aId.eq(Long.parseLong("1"))).fetch();
-		System.out.println("select a.*,b.* from test.atable a inner join  test.btable b on b.a_table_a_id=a.a_id where a.a_id=1");
-		System.out.println("a테이블값만: "+aTables.get(0).getAId());
-		//join값 조회시 select로 조회 즉 필요할때 가져옴 ㅎㄷㄷ 하다 
-		for(aTable a:aTables){
-			List<bTable>btables=a.getBTables();
-			for(bTable b:btables){
-				System.out.println("b테이블값 조인하기: "+b.getReply());
-			}
-		}
-		System.out.println("-----------------------------------------------------------------");
-
-	}
-	@Test
 	void test2(){
 		QaTable qaTable=QaTable.aTable;
 		QbTable qbTable=QbTable.bTable;
@@ -75,7 +57,8 @@ class SttudyJpaApplicationTests {
 	void test5(){
 		QaTable qaTable=QaTable.aTable;
 		QbTable qbTable=QbTable.bTable;
-		List<Tuple>tuples=jpaQueryFactory.select(qaTable,qbTable).from(qaTable).leftJoin(qbTable).on(qbTable.aTable.aId.eq(qaTable.aId)).where(qaTable.aId.eq(Long.parseLong("1"))).orderBy(qbTable.Id.desc()).offset(getStart(2, 2)-1).limit(2).fetch();
+		List<Tuple>tuples=jpaQueryFactory.select(qaTable,qbTable).from(qaTable).leftJoin(qbTable).on(qbTable.aTable.aId.eq(qaTable.aId))
+		.where(qaTable.aId.eq(Long.parseLong("1"))).orderBy(qbTable.Id.desc()).offset(getStart(2, 2)-1).limit(2).fetch();
 		//넣는 객체대로 get/set메소드가 생김 실화임?
 		for(Tuple t:tuples){
 			System.out.println(t.get(qaTable).getText());
